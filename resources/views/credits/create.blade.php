5dff4a33cd8e5f92a26155c4d163f2c19c55f0f5
Fincomunidad <lalomijafru126@gmail.com>
2024-09-24 09:04:08 -0600

    x

--- resources/views/credits/create.blade.php
+++ resources/views/credits/create.blade.php
@@ -94,3 +94,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+<<<<<<< HEAD
+=======
+
+
+falta que funcione
+>>>>>>> parent of f700570 (correciones)
